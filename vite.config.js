11528f20e94252aa0c280bcea686d0f2df55103a
RobyDoby <jettann94@gmail.com>
2022-09-29 22:55:00 +0300

    added starting files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	build: {
+		target: 'es2017',
+		outDir: 'build',
+		rollupOptions: {
+			output: {
+				assetFileNames: ({ name }) => {
+					if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+						return 'assets/images/[name]-[hash][extname]';
+					}
+
+					if (/\.(ttf|otf|fnt|woff|woff2)$/.test(name ?? '')) {
+						return 'assets/fonts/[name]-[hash][extname]';
+					}
+
+					if (/\.css$/.test(name ?? '')) {
+						return 'assets/css/[name]-[hash][extname]';
+					}
+
+					return 'assets/[name]-[hash][extname]';
+				},
+			},
+		},
+	},
+	server: {
+		port: 3000,
+		host: '0.0.0.0',
+		hmr: true,
+	},
+	plugins: [
+		react(),
+		legacy({
+			targets: ['defaults', 'not IE 11'],
+		}),
+	],
+});
